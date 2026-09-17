1c247d7540f4a24f26a6098db02028088d56777e
SILA Network <pssd500@gmail.com>
2026-09-17 22:29:50 +0300

    core: fix goimports ordering

--- core/overlay/state_transition.go
+++ core/overlay/state_transition.go
@@ -22,8 +22,8 @@ import (
 
 	"github.com/sila-chain/go-sila/common"
 	"github.com/sila-chain/go-sila/core/rawdb"
-	"github.com/sila-chain/go-sila/sildb"
 	"github.com/sila-chain/go-sila/log"
+	"github.com/sila-chain/go-sila/sildb"
 )
 
 // TransitionState is a structure that holds the progress markers of the
