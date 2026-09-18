a37086b72b15f5d3952d22aebe467c15b20c1f2e
SILA Network <pssd500@gmail.com>
2026-09-18 19:29:33 +0300

    node, p2p: normalize imports

--- p2p/nat/nat.go
+++ p2p/nat/nat.go
@@ -25,8 +25,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/sila-chain/go-sila/log"
 	natpmp "github.com/jackpal/go-nat-pmp"
+	"github.com/sila-chain/go-sila/log"
 )
 
 // Interface An implementation of nat.Interface can map local ports to ports
