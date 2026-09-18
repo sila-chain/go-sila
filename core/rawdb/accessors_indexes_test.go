2d5e9beb51e128c926ff94e2cb88baff693f77dd
SILA Network <pssd500@gmail.com>
2026-09-18 17:36:47 +0300

    core/rawdb: normalize accessor imports

--- core/rawdb/accessors_indexes_test.go
+++ core/rawdb/accessors_indexes_test.go
@@ -22,12 +22,12 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/holiman/uint256"
 	"github.com/sila-chain/go-sila/common"
 	"github.com/sila-chain/go-sila/core/types"
-	"github.com/sila-chain/go-sila/sildb"
 	"github.com/sila-chain/go-sila/internal/blocktest"
 	"github.com/sila-chain/go-sila/rlp"
-	"github.com/holiman/uint256"
+	"github.com/sila-chain/go-sila/sildb"
 )
 
 var newTestHasher = blocktest.NewHasher
