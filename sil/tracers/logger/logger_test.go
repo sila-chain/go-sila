8680498d6a077bbcab5b8d81f29eb134ef03b36c
SILA Network <pssd500@gmail.com>
2026-09-18 23:29:37 +0300

    sil/tracers: normalize js and logger imports

--- sil/tracers/logger/logger_test.go
+++ sil/tracers/logger/logger_test.go
@@ -22,11 +22,11 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/holiman/uint256"
 	"github.com/sila-chain/go-sila/common"
 	"github.com/sila-chain/go-sila/core/state"
 	"github.com/sila-chain/go-sila/core/vm"
 	"github.com/sila-chain/go-sila/params"
-	"github.com/holiman/uint256"
 )
 
 type dummyStatedb struct {
