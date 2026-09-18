f965ec273e2edeffe62357fe21fa16e9433c0a3e
SILA Network <pssd500@gmail.com>
2026-09-18 19:18:45 +0300

    core/types: normalize imports

--- core/types/rlp_fuzzer_test.go
+++ core/types/rlp_fuzzer_test.go
@@ -22,8 +22,8 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/sila-chain/go-sila/rlp"
 	"github.com/holiman/uint256"
+	"github.com/sila-chain/go-sila/rlp"
 )
 
 func decodeEncode(input []byte, val interface{}) error {
