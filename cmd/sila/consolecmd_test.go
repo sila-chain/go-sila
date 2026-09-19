7ce6ef39d4bc7d43b316d93286a16006862149c7
SILA Network <pssd500@gmail.com>
2026-09-19 18:14:03 +0300

    test: align Windows amd64 expectations with Sila

--- cmd/sila/consolecmd_test.go
+++ cmd/sila/consolecmd_test.go
@@ -30,8 +30,8 @@ import (
 )
 
 const (
-	ipcAPIs  = "admin:1.0 debug:1.0 engine:1.0 sil:1.0 miner:1.0 net:1.0 rpc:1.0 testing:1.0 txpool:1.0 web3:1.0"
-	httpAPIs = "sil:1.0 net:1.0 rpc:1.0 web3:1.0"
+	ipcAPIs  = "admin:1.0 debug:1.0 engine:1.0 miner:1.0 net:1.0 rpc:1.0 sil:1.0 testing:1.0 txpool:1.0 web3:1.0"
+	httpAPIs = "net:1.0 rpc:1.0 sil:1.0 web3:1.0"
 )
 
 // spawns sila with the given command line args, using a set of flags to minimise
