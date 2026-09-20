f9073d605ec4873d8b5ff22ce6599041788f1f4a
SILA Network <pssd500@gmail.com>
2026-09-20 23:12:25 +0300

    test: match canonical execution-spec fixture paths

--- tests/block_test.go
+++ tests/block_test.go
@@ -91,12 +91,12 @@ func TestExecutionSpecBlocktests(t *testing.T) {
 	bt := new(testMatcher)
 
 	// These tests require us to handle scenarios where a system contract is not deployed at a fork
-	bt.skipLoad(`.*sip7251_consolidations/contract_deployment/system_contract_deployment\.json`)
-	bt.skipLoad(`.*sip7002_el_triggerable_withdrawals/contract_deployment/system_contract_deployment\.json`)
+	bt.skipLoad(`.*eip7251_consolidations/contract_deployment/system_contract_deployment\.json`)
+	bt.skipLoad(`.*eip7002_el_triggerable_withdrawals/contract_deployment/system_contract_deployment\.json`)
 
 	// Broken tests
-	bt.skipLoad(`.*sip7610_create_collision/initcollision/.*`)
-	bt.skipLoad(`.*sip7610_create_collision/revert_in_create/.*`)
+	bt.skipLoad(`.*eip7610_create_collision/initcollision/.*`)
+	bt.skipLoad(`.*eip7610_create_collision/revert_in_create/.*`)
 
 	bt.walk(t, executionSpecBlockchainTestDir, func(t *testing.T, name string, test *BlockTest) {
 		execBlockTest(t, bt, test)
