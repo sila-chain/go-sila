392df07200cf8af60ee287c3d40d53955254401c
SILA Network <pssd500@gmail.com>
2026-09-22 22:17:48 +0300

    fix: deduplicate imports in sync_test.go

--- core/state/sync_test.go
+++ core/state/sync_test.go
@@ -32,16 +32,6 @@ import (
 	"github.com/sila-chain/go-sila/triedb/hashdb"
 	"github.com/sila-chain/go-sila/triedb/pathdb"
 	"github.com/holiman/uint256"
-	"github.com/sila-chain/go-sila/common"
-	"github.com/sila-chain/go-sila/core/rawdb"
-	"github.com/sila-chain/go-sila/core/types"
-	"github.com/sila-chain/go-sila/crypto"
-	"github.com/sila-chain/go-sila/rlp"
-	"github.com/sila-chain/go-sila/sildb"
-	"github.com/sila-chain/go-sila/trie"
-	"github.com/sila-chain/go-sila/triedb"
-	"github.com/sila-chain/go-sila/triedb/hashdb"
-	"github.com/sila-chain/go-sila/triedb/pathdb"
 )
 
 // testAccount is the data associated with an account used by the state tests.
