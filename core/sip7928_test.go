c4c211e3604ac916de4668d7f76d6a2c93d5efe9
SILA Network <pssd500@gmail.com>
2026-09-22 22:17:31 +0300

    fix: deduplicate imports in sip7928_test.go

--- core/sip7928_test.go
+++ core/sip7928_test.go
@@ -36,13 +36,6 @@ import (
 	"github.com/sila-chain/go-sila/params"
 	"github.com/sila-chain/go-sila/trie"
 	"github.com/holiman/uint256"
-	"github.com/sila-chain/go-sila/common"
-	"github.com/sila-chain/go-sila/consensus/beacon"
-	"github.com/sila-chain/go-sila/consensus/silash"
-	"github.com/sila-chain/go-sila/core/types"
-	"github.com/sila-chain/go-sila/core/types/bal"
-	"github.com/sila-chain/go-sila/crypto"
-	"github.com/sila-chain/go-sila/params"
 )
 
 // SIP-7928 BAL inclusion tests.
