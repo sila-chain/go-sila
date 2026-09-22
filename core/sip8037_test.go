a8d62d8957a13cdd9630eb4f01ff0b25e76af276
SILA Network <pssd500@gmail.com>
2026-09-22 22:17:35 +0300

    fix: deduplicate imports in sip8037_test.go

--- core/sip8037_test.go
+++ core/sip8037_test.go
@@ -37,15 +37,6 @@ import (
 	"github.com/sila-chain/go-sila/crypto"
 	"github.com/sila-chain/go-sila/params"
 	"github.com/holiman/uint256"
-	"github.com/sila-chain/go-sila/common"
-	"github.com/sila-chain/go-sila/consensus/beacon"
-	"github.com/sila-chain/go-sila/consensus/silash"
-	"github.com/sila-chain/go-sila/core/state"
-	"github.com/sila-chain/go-sila/core/tracing"
-	"github.com/sila-chain/go-sila/core/types"
-	"github.com/sila-chain/go-sila/core/vm"
-	"github.com/sila-chain/go-sila/crypto"
-	"github.com/sila-chain/go-sila/params"
 )
 
 var (
