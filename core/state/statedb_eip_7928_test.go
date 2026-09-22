0f66e3ed9da5ea1bed9e8403ae169b1d401f3d6f
SILA Network <pssd500@gmail.com>
2026-09-22 22:59:31 +0300

    tests: format Sila EIP-7928 state test imports

--- core/state/statedb_eip_7928_test.go
+++ core/state/statedb_eip_7928_test.go
@@ -19,12 +19,12 @@ package state
 import (
 	"testing"
 
+	"github.com/holiman/uint256"
 	"github.com/sila-chain/go-sila/common"
 	"github.com/sila-chain/go-sila/core/tracing"
 	"github.com/sila-chain/go-sila/core/types"
 	"github.com/sila-chain/go-sila/core/types/bal"
 	"github.com/sila-chain/go-sila/params"
-	"github.com/holiman/uint256"
 )
 
 // TestApplyBlockAccessListConcurrentPrefetch stresses the concurrent storage
