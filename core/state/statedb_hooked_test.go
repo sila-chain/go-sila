7fdaff68235fa42fb26c6790089105d546bfb5a4
SILA Network <pssd500@gmail.com>
2026-09-22 22:17:39 +0300

    fix: deduplicate imports in statedb_hooked_test.go

--- core/state/statedb_hooked_test.go
+++ core/state/statedb_hooked_test.go
@@ -26,9 +26,6 @@ import (
 	"github.com/sila-chain/go-sila/core/types"
 	"github.com/sila-chain/go-sila/params"
 	"github.com/holiman/uint256"
-	"github.com/sila-chain/go-sila/common"
-	"github.com/sila-chain/go-sila/core/tracing"
-	"github.com/sila-chain/go-sila/core/types"
 )
 
 // This method tests that the 'burn' from sending-to-selfdestructed accounts
